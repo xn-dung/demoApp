1616bbee265b1000654103ccab393942a96902de
Lê Dũng <dungcobachi2003@gmail.com>
2025-09-17 13:39:46 +0700

    đã chỉnh dao

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("mysql:mysql-connector-java:8.1.0")
+
 }
--- app/src/main/java/com/example/myapplication/data/dao/DAO.java
+++ app/src/main/java/com/example/myapplication/data/dao/DAO.java
@@ -1,4 +1,26 @@
 package com.example.myapplication.data.dao;
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DAO {
+    public static Connection con;
+
+    public DAO() {
+        if (con == null) {
+            String dbUrl = "jdbc:mysql://localhost:3306/appProject?autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true&characterEncoding=UTF-8";
+            String dbClass = "com.mysql.cj.jdbc.Driver";
+
+            try {
+                Class.forName(dbClass);
+                con = DriverManager.getConnection(dbUrl, "sa", "1");
+                System.out.println("Kết nối CSDL thành công");
+            } catch (Exception e) {
+                System.out.println("Kết nối CSDL thất bại");
+                e.printStackTrace();
+            }
+        }
+    }
 }
